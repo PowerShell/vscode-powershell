f3ff512b2b2010ee148ea2694e0e2173e4921d65
dfinke <finked@hotmail.com>
2015-12-02 09:53:18 -0500

    Updated

--- src/features/ExpandAlias.ts
+++ src/features/ExpandAlias.ts
@@ -12,10 +12,23 @@ export function registerExpandAliasCommand(client: LanguageClient): void {
 
 		var editor    = Window.activeTextEditor;
 		var document  = editor.document;		
-		var selection = editor.selection;		
-		var text      = document.getText(selection);
-		var range=new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
-				
+		var selection = editor.selection;	
+		var text, range;
+		
+		var sls = selection.start;
+		var sle = selection.end;
+		
+		if(
+			(sls.character === sle.character ) &&
+			(sls.line === sle.line)
+		) {
+			text  = document.getText();		
+			range = new vscode.Range(0, 0, document.lineCount, text.length);		
+		} else {
+			text  = document.getText(selection);		
+			range = new vscode.Range(sls.line, sls.character, sle.line, sle.character);		
+		}
+						
 		client.sendRequest(ExpandAliasRequest.type, text).then((result) => {		
 			editor.edit((editBuilder) => {				
 				editBuilder.replace(range, result);
