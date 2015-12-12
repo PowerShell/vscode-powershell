7ac98e014851f85e506d09e5b0d2234780834636
David Wilson <daviwil@microsoft.com>
2015-12-11 16:30:33 -0800

    Fix #48: Implement new deactivate method
    This change adds the new deactivate method which gets called when an
    extension should shut itself down.  This will be used to unload this
    extension before an update gets installed by the user in VS Code.

--- src/main.ts
+++ src/main.ts
@@ -14,42 +14,44 @@ import { registerExpandAliasCommand } from './features/ExpandAlias';
 import { registerShowHelpCommand } from './features/ShowOnlineHelp';
 import { registerConsoleCommands } from './features/Console';
 
+var languageServerClient : LanguageClient = undefined;
+
 export function activate(context: vscode.ExtensionContext): void {
-	
+
 	var PowerShellLanguageId = 'powershell';
 	var settings = settingsManager.load('powershell');
 
-    vscode.languages.setLanguageConfiguration(PowerShellLanguageId, 
+    vscode.languages.setLanguageConfiguration(PowerShellLanguageId,
 	{
 		wordPattern: /(-?\d*\.\d\w*)|([^\`\~\!\@\#\%\^\&\*\(\)\=\+\[\{\]\}\\\|\;\'\"\,\.\<\>\/\?\s]+)/g,
-		
+
 		indentationRules: {
 			// ^(.*\*/)?\s*\}.*$
 			decreaseIndentPattern: /^(.*\*\/)?\s*\}.*$/,
 			// ^.*\{[^}"']*$
 			increaseIndentPattern: /^.*\{[^}"']*$/
-		},		
-		
+		},
+
 		comments: {
 			lineComment: '#',
 			blockComment: ['<#', '#>']
 		},
-		
+
 		brackets: [
 			['{', '}'],
 			['[', ']'],
 			['(', ')'],
 		],
-		
+
 		__electricCharacterSupport: {
 			brackets: [
 				{ tokenType:'delimiter.curly.ts', open: '{', close: '}', isElectric: true },
 				{ tokenType:'delimiter.square.ts', open: '[', close: ']', isElectric: true },
 				{ tokenType:'delimiter.paren.ts', open: '(', close: ')', isElectric: true }
 			],
-			docComment: { scope:'comment.documentation', open:'/**', lineStart:' * ', close:' */' }			
+			docComment: { scope:'comment.documentation', open:'/**', lineStart:' * ', close:' */' }
 		},
-		
+
 		__characterPairSupport: {
 			autoClosingPairs: [
 				{ open: '{', close: '}' },
@@ -57,23 +59,23 @@ export function activate(context: vscode.ExtensionContext): void {
 				{ open: '(', close: ')' },
 				{ open: '"', close: '"', notIn: ['string'] },
 				{ open: '\'', close: '\'', notIn: ['string', 'comment'] }
-			]			
+			]
 		}
 	});
-	
+
 	let args = [];
 	if (settings.developer.editorServicesWaitForDebugger)
 	{
 		args.push('/waitForDebugger');
 	}
-	
+
 	let serverPath = resolveLanguageServerPath(settings);
 	let serverOptions = {
-		run: { 
+		run: {
 			command: serverPath,
-			args: args 
+			args: args
 		},
-		debug: { 
+		debug: {
 			command: serverPath,
 			args: ['/waitForDebugger']
 		}
@@ -87,33 +89,42 @@ export function activate(context: vscode.ExtensionContext): void {
 		}
 	}
 
-	let client = 
+	languageServerClient =
 		new LanguageClient(
-			'PowerShell Editor Services', 
-			serverOptions, 
+			'PowerShell Editor Services',
+			serverOptions,
 			clientOptions);
-			
-    client.start();
+
+    languageServerClient.start();
 
 	// Register other features
-	registerExpandAliasCommand(client);
-	registerShowHelpCommand(client);
-	registerConsoleCommands(client);
+	registerExpandAliasCommand(languageServerClient);
+	registerShowHelpCommand(languageServerClient);
+	registerConsoleCommands(languageServerClient);
+}
+
+export function deactivate(): void {
+	if (languageServerClient)
+	{
+		// Close the language server client
+		languageServerClient.stop();
+		languageServerClient = undefined;
+	}
 }
 
 function resolveLanguageServerPath(settings: settingsManager.ISettings) : string {
 	var editorServicesHostPath = settings.developer.editorServicesHostPath;
-	
+
 	if (editorServicesHostPath)
-	{	
+	{
 		console.log("Found Editor Services path from config: " + editorServicesHostPath);
-				
+
 		// Make the path absolute if it's not
 		editorServicesHostPath =
 			path.resolve(
 				__dirname,
 				editorServicesHostPath);
-				
+
 		console.log("    Resolved path to: " + editorServicesHostPath);
 	}
 	else
@@ -125,9 +136,9 @@ function resolveLanguageServerPath(settings: settingsManager.ISettings) : string
 				'..',
 				'bin',
 				'Microsoft.PowerShell.EditorServices.Host.exe');
-				
+
 		console.log("Using default Editor Services path: " + editorServicesHostPath);
 	}
-	
+
 	return editorServicesHostPath;
 }
