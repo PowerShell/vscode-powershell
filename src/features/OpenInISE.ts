4a33f1b6e72d9d0c041946ea79843572fcef8686
Jan Egil Ring <jan.egil.ring@outlook.com>
2016-01-10 23:30:00 +0100

    Implemented processor architecture check based on http://blog.differentpla.net/blog/2013/03/10/processor-architew6432

--- src/features/OpenInISE.ts
+++ src/features/OpenInISE.ts
@@ -1,6 +1,5 @@
 import vscode = require('vscode'); 
 import Window = vscode.window;
-import os = require('os'); 
 
 export function registerOpenInISECommand(): void {
     var disposable = vscode.commands.registerCommand('PowerShell.OpenInISE', () => {
@@ -10,27 +9,17 @@ export function registerOpenInISECommand(): void {
 
 var uri = document.uri
 
-// For testing, to be removed
-Window.showInformationMessage(uri.fsPath);
-Window.showInformationMessage(uri.path);
-
-if (os.arch()== 'x64') {
+if (process.env.hasOwnProperty('PROCESSOR_ARCHITEW6432')) {
     
     var ISEPath = 'C:\\Windows\\Sysnative\\WindowsPowerShell\\v1.0\\powershell_ise.exe';
 
 } else
 {
 
-    // Need to figure out how to check OS architecture os.arch returns process architecture
-    //var ISEPath = process.env.windir + '\\System32\\WindowsPowerShell\\v1.0\\powershell_ise.exe';
-    var ISEPath = 'C:\\Windows\\Sysnative\\WindowsPowerShell\\v1.0\\powershell_ise.exe';
+    var ISEPath = process.env.windir + '\\System32\\WindowsPowerShell\\v1.0\\powershell_ise.exe';
 
 }
-
-// For testing, to be removed
-Window.showInformationMessage(os.arch());
-Window.showInformationMessage(ISEPath);
-        
+       
         require("child_process").exec(ISEPath + ' -NoProfile -File ' + uri.fsPath).unref();
         
 
