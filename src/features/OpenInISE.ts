350448f1172f5a25ee0cda6d632d02ba9611b84a
Jan Egil Ring <jan.egil.ring@outlook.com>
2016-01-10 22:54:55 +0100

    Removed convertUriToPath due to the fact that vscode.window.activeTextEditor.editor.document.uri got an fsPath property which can be used instead. Path to ISE is now set as a variable which will be assigned based on OS architecture.

--- src/features/OpenInISE.ts
+++ src/features/OpenInISE.ts
@@ -1,30 +1,37 @@
 import vscode = require('vscode'); 
 import Window = vscode.window;
-
-
-/** 
-function convertUriToPath(Uri) {
-  
-  window.showInformationMessage(Uri)
-  var result = Uri.replace("file://","")
-  var result = result.replace("/","\")
-  var driveletter = result.substring(0)
-  var result =   driveletter + ":" + result.substring(1,-1)
-  
-}
-*/
+import os = require('os'); 
 
 export function registerOpenInISECommand(): void {
     var disposable = vscode.commands.registerCommand('PowerShell.OpenInISE', () => {
 
 		var editor    = Window.activeTextEditor;
 		var document  = editor.document;		
+
+var uri = document.uri
+
+// For testing, to be removed
+Window.showInformationMessage(uri.fsPath);
+Window.showInformationMessage(uri.path);
+
+if (os.arch()== 'x64') {
+    
+    var ISEPath = 'C:\\Windows\\Sysnative\\WindowsPowerShell\\v1.0\\powershell_ise.exe';
+
+} else
+{
+
+    // Need to figure out how to check OS architecture os.arch returns process architecture
+    //var ISEPath = process.env.windir + '\\System32\\WindowsPowerShell\\v1.0\\powershell_ise.exe';
+    var ISEPath = 'C:\\Windows\\Sysnative\\WindowsPowerShell\\v1.0\\powershell_ise.exe';
+
+}
+
+// For testing, to be removed
+Window.showInformationMessage(os.arch());
+Window.showInformationMessage(ISEPath);
         
-        //process = require('child_process');
-        //var filePath = convertUriToPath(editor.document.uri);
-        var filePath = 'C:\temp\Get-ProductKey.ps1'
-        //require("child_process").exec('powershell_ise.exe -NoProfile -File "C:\\temp\\Get-ProductKey.ps1"').unref();
-        require("child_process").exec(process.env.windir + '\\System32\\WindowsPowerShell\\v1.0\\powershell_ise.exe -NoProfile -File "C:\\temp\\Get-ProductKey.ps1"').unref();
+        require("child_process").exec(ISEPath + ' -NoProfile -File ' + uri.fsPath).unref();
         
 
 			});
