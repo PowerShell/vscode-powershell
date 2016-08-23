40bca0aab10123334be2bed26c6582afa7a619c9
David Wilson <daviwil@microsoft.com>
2016-08-23 16:46:28 -0700

    Update PowerShell Editor Services version

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ import net = require('net');
 
 // NOTE: We will need to find a better way to deal with the required
 //       PS Editor Services version...
-var requiredEditorServicesVersion = "0.7.0";
+var requiredEditorServicesVersion = "0.7.1";
 
 var powerShellProcess: cp.ChildProcess = undefined;
 var languageServerClient: LanguageClient = undefined;
