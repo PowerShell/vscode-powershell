1667eef999fe213320bc60467f01d941824cf914
Kapil Borle <kborle@microsoft.com>
2016-12-16 23:43:32 -0800

    Allow quick fix to work in untitled documents
    In the previous iteration of this feature, the edits can be applied to files open in a workspace. This commit removes that restriction.

--- src/features/CodeActions.ts
+++ src/features/CodeActions.ts
@@ -9,21 +9,15 @@ export class CodeActionsFeature implements IFeature {
 
     constructor() {
         this.command = vscode.commands.registerCommand('PowerShell.ApplyCodeActionEdits', (edit: any) => {
-            var editor = Window.activeTextEditor;
-            var filePath = editor.document.fileName;
-            var workspaceEdit = new vscode.WorkspaceEdit();
-            workspaceEdit.set(
-                vscode.Uri.file(filePath),
-                [
-                    new vscode.TextEdit(
-                        new vscode.Range(
-                            edit.StartLineNumber - 1,
-                            edit.StartColumnNumber - 1,
-                            edit.EndLineNumber - 1,
-                            edit.EndColumnNumber - 1),
-                        edit.Text)
-                ]);
-            vscode.workspace.applyEdit(workspaceEdit);
+            Window.activeTextEditor.edit((editBuilder) => {
+                editBuilder.replace(
+                    new vscode.Range(
+                        edit.StartLineNumber - 1,
+                        edit.StartColumnNumber - 1,
+                        edit.EndLineNumber - 1,
+                        edit.EndColumnNumber - 1),
+                    edit.Text);
+            });
         });
     }
 
