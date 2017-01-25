e592d5a941a7481310a10bbb0b55823d94aee94a
Kapil Borle <kborle@microsoft.com>
2017-01-24 23:10:06 -0800

    Add copyright to anmiatedStatusBar.ts

--- src/animatedStatusBar.ts
+++ src/animatedStatusBar.ts
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+
 import {
     StatusBarItem,
     StatusBarAlignment,
