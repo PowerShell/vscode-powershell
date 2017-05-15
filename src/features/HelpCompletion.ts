07501ae3d4b5ba086e7651f054b66f3201406b4d
Kapil Borle <kborle@microsoft.com>
2017-05-15 13:52:51 -0700

    Change trigger characters from "#<" to "<#"

--- src/features/HelpCompletion.ts
+++ src/features/HelpCompletion.ts
@@ -36,7 +36,7 @@ export class HelpCompletionFeature implements IFeature {
     }
 
     constructor() {
-        this.triggerCharactersBlockComment = "#<";
+        this.triggerCharactersBlockComment = "<#";
         this.triggerCharactersLineComment = "##";
         let subscriptions = [];
         workspace.onDidChangeTextDocument(this.onEvent, this, subscriptions);
@@ -44,8 +44,8 @@ export class HelpCompletionFeature implements IFeature {
         this.disposable = Disposable.from(...subscriptions);
     }
 
-    setLanguageClient(languageclient: LanguageClient) {
-        this.languageClient = languageclient;
+    setLanguageClient(languageClient: LanguageClient) {
+        this.languageClient = languageClient;
     }
 
     dispose() {
@@ -56,15 +56,20 @@ export class HelpCompletionFeature implements IFeature {
         let text = changeEvent.contentChanges[0].text;
         switch (this.searchState) {
             case SearchState.Searching:
-                if (text.length === 1 && text[0] === this.triggerCharactersBlockComment[0]) {
-                    this.searchState = SearchState.Locked;
+                if (text.length === 1) {
+                    if (text[0] === this.triggerCharactersBlockComment[0]) {
+                        this.searchState = SearchState.Locked;
+                        this.triggerCharactersFound = this.triggerCharactersBlockComment;
+                    }
+                    else if (text[0] === this.triggerCharactersLineComment[0]) {
+                        this.searchState = SearchState.Locked;
+                        this.triggerCharactersFound = this.triggerCharactersLineComment;
+                    }
                 }
                 break;
 
             case SearchState.Locked:
-                if (text.length === 1 &&
-                    (text[0] === (this.triggerCharactersFound = this.triggerCharactersBlockComment)[1] ||
-                        text[0] === (this.triggerCharactersFound = this.triggerCharactersLineComment)[1])) {
+                if (text.length === 1 && text[0] === this.triggerCharactersFound[1]) {
                     this.searchState = SearchState.Found;
                 }
                 else {
@@ -89,18 +94,22 @@ export class HelpCompletionFeature implements IFeature {
                         triggerPosition: triggerStartPos,
                         blockComment: this.isBlockComment
                     }).then(result => {
+                        if (result === undefined) {
+                            return;
+                        }
+
                         let content = result.content;
                         if (content === undefined) {
                             return;
                         }
 
-                        // todo allow "##" as trigger characters
                         // todo remove the new line after help block
                         // todo get the eol character programmatically or let the server return one whole string
+                        // todo add indentation level to the help content
                         let editor = vscode.window.activeTextEditor;
                         let replaceRange = new vscode.Range(triggerStartPos.translate(0, -1), triggerStartPos.translate(0, 1));
                         let text = content.join("\r\n");
-                        editor.edit(editbuilder => editbuilder.replace(replaceRange, text));
+                        editor.edit(editBuilder => editBuilder.replace(replaceRange, text));
                     });
             }
         }
