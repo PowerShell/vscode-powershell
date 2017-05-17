8ba1e21b3442feccc2d94de0f6fa0db874dc5dea
Kapil Borle <kborle@microsoft.com>
2017-05-16 20:35:05 -0700

    Componentize HelpCompletion feature

--- src/features/HelpCompletion.ts
+++ src/features/HelpCompletion.ts
@@ -3,7 +3,7 @@
  *--------------------------------------------------------*/
 
 import { IFeature } from "../feature";
-import { TextDocumentChangeEvent, workspace, Disposable, Position, window, Range, EndOfLine, SnippetString } from "vscode";
+import { TextDocumentChangeEvent, workspace, Disposable, Position, window, Range, EndOfLine, SnippetString, TextDocument } from "vscode";
 import { LanguageClient, RequestType } from "vscode-languageclient";
 
 export namespace CommentHelpRequest {
@@ -23,28 +23,20 @@ interface CommentHelpRequestResult {
 enum SearchState { Searching, Locked, Found };
 
 export class HelpCompletionFeature implements IFeature {
-    private readonly triggerCharactersBlockComment: string;
-    private readonly triggerCharactersLineComment: string;
-    private triggerCharactersFound: string;
+    private helpCompletionProvider: HelpCompletionProvider;
     private languageClient: LanguageClient;
     private disposable: Disposable;
-    private searchState: SearchState;
-    private get isBlockComment(): boolean {
-        return this.triggerCharactersFound !== undefined &&
-            this.triggerCharactersFound === this.triggerCharactersBlockComment;
-    }
 
     constructor() {
-        this.triggerCharactersBlockComment = "<#";
-        this.triggerCharactersLineComment = "##";
+        this.helpCompletionProvider = new HelpCompletionProvider();
         let subscriptions = [];
         workspace.onDidChangeTextDocument(this.onEvent, this, subscriptions);
-        this.searchState = SearchState.Searching;
         this.disposable = Disposable.from(...subscriptions);
     }
 
     setLanguageClient(languageClient: LanguageClient) {
         this.languageClient = languageClient;
+        this.helpCompletionProvider.languageClient = languageClient;
     }
 
     dispose() {
@@ -52,69 +44,134 @@ export class HelpCompletionFeature implements IFeature {
     }
 
     onEvent(changeEvent: TextDocumentChangeEvent): void {
-        // todo split this method into logical components
-        // todo create a helpcompletion provider class
-        // todo associate state with a given document
-        let text = changeEvent.contentChanges[0].text;
-        switch (this.searchState) {
+        this.helpCompletionProvider.updateState(
+            changeEvent.document,
+            changeEvent.contentChanges[0].text,
+            changeEvent.contentChanges[0].range);
+
+        // todo raise an event when trigger is found, and attach complete() to the event.
+        if (this.helpCompletionProvider.triggerFound) {
+            this.helpCompletionProvider.reset();
+            this.helpCompletionProvider.complete();
+        }
+
+    }
+}
+
+class TriggerFinder {
+    private state: SearchState;
+    private document: TextDocument;
+    private count: number;
+    constructor(private triggerCharacters: string) {
+        this.state = SearchState.Searching;
+        this.count = 0;
+    }
+
+    public get found(): boolean {
+        return this.state === SearchState.Found;
+    }
+
+    public updateState(document: TextDocument, changeText: string): void {
+        switch (this.state) {
             case SearchState.Searching:
-                if (text.length === 1) {
-                    if (text[0] === this.triggerCharactersBlockComment[0]) {
-                        this.searchState = SearchState.Locked;
-                        this.triggerCharactersFound = this.triggerCharactersBlockComment;
-                    }
-                    else if (text[0] === this.triggerCharactersLineComment[0]) {
-                        this.searchState = SearchState.Locked;
-                        this.triggerCharactersFound = this.triggerCharactersLineComment;
-                    }
+                if (changeText.length === 1 && changeText[0] === this.triggerCharacters[this.count]) {
+                    this.state = SearchState.Locked;
+                    this.document = document;
+                    this.count++;
                 }
                 break;
 
             case SearchState.Locked:
-                if (text.length === 1 && text[0] === this.triggerCharactersFound[1]) {
-                    this.searchState = SearchState.Found;
+                if (changeText.length === 1 && changeText[0] === this.triggerCharacters[this.count] && document === this.document) {
+                    this.count++;
+                    if (this.count === this.triggerCharacters.length) {
+                        this.state = SearchState.Found;
+                    }
                 }
                 else {
-                    this.searchState = SearchState.Searching;
+                    this.reset();
                 }
                 break;
+
+            default:
+                this.reset();
+                break;
+        }
+    }
+
+    public reset(): void {
+        this.state = SearchState.Searching;
+        this.count = 0;
+    }
+}
+
+class HelpCompletionProvider {
+    private triggerFinderBlockComment: TriggerFinder;
+    private triggerFinderLineComment: TriggerFinder;
+    private lastChangeText: string;
+    private lastChangeRange: Range;
+    private lastDocument: TextDocument;
+    private langClient: LanguageClient;
+
+    constructor() {
+        this.triggerFinderBlockComment = new TriggerFinder("<#");
+        this.triggerFinderLineComment = new TriggerFinder("##");
+    }
+
+    public get triggerFound(): boolean {
+        return this.triggerFinderBlockComment.found || this.triggerFinderLineComment.found;
+    }
+
+    public set languageClient(value: LanguageClient) {
+        this.langClient = value;
+    }
+
+    public updateState(document: TextDocument, changeText: string, changeRange: Range): void {
+        this.lastDocument = document;
+        this.lastChangeText = changeText;
+        this.lastChangeRange = changeRange;
+        this.triggerFinderBlockComment.updateState(document, changeText);
+        this.triggerFinderLineComment.updateState(document, changeText);
+    }
+
+    public reset(): void {
+        this.triggerFinderBlockComment.reset();
+        this.triggerFinderLineComment.reset();
+    }
+
+    public complete(): Thenable<void> {
+        if (this.langClient === undefined) {
+            return;
         }
 
-        let r = changeEvent.contentChanges[0].range;
-        console.log(`Search State: ${this.searchState.toString()}; Range: (${r.start.line}, ${r.start.character}), (${r.end.line}, ${r.end.character})`);
-        if (this.searchState === SearchState.Found) {
-            this.searchState = SearchState.Searching;
-            if (this.languageClient) {
-                let change = changeEvent.contentChanges[0];
-                let triggerStartPos = change.range.start;
-                let triggerEndPos = change.range.end;
-                let doc = changeEvent.document;
-                this.languageClient.sendRequest(
-                    CommentHelpRequest.type,
-                    {
-                        documentUri: changeEvent.document.uri.toString(),
-                        triggerPosition: triggerStartPos,
-                        blockComment: this.isBlockComment
-                    }).then(result => {
-                        if (result === undefined) {
-                            return;
-                        }
+        let change = this.lastChangeText;
+        let triggerStartPos = this.lastChangeRange.start;
+        let triggerEndPos = this.lastChangeRange.end;
+        let doc = this.lastDocument;
+        this.langClient.sendRequest(
+            CommentHelpRequest.type,
+            {
+                documentUri: doc.uri.toString(),
+                triggerPosition: triggerStartPos,
+                blockComment: this.triggerFinderBlockComment.found
+            }).then(result => {
+                if (result === undefined) {
+                    return;
+                }
 
-                        let content = result.content;
-                        if (content === undefined) {
-                            return;
-                        }
+                let content = result.content;
+                if (content === undefined) {
+                    return;
+                }
 
-                        // todo add indentation level to the help content
-                        let editor = window.activeTextEditor;
-                        let replaceRange = new Range(triggerStartPos.translate(0, -1), triggerStartPos.translate(0, 1));
+                // todo add indentation level to the help content
+                let editor = window.activeTextEditor;
+                let replaceRange = new Range(triggerStartPos.translate(0, -1), triggerStartPos.translate(0, 1));
 
-                        // Trim the last empty line and join the strings.
-                        let text = content.slice(0, -1).join(this.getEOL(doc.eol));
-                        editor.insertSnippet(new SnippetString(text), replaceRange);
-                    });
-            }
-        }
+                // Trim the last empty line and join the strings.
+                let text = content.slice(0, -1).join(this.getEOL(doc.eol));
+                editor.insertSnippet(new SnippetString(text), replaceRange);
+            });
     }
 
     private getEOL(eol: EndOfLine): string {
