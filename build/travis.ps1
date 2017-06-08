2352880b2f3cc5f4b700bc820f8f3377e7dd1702
David Wilson <daviwil@microsoft.com>
2017-06-08 05:53:31 -0700

    Add missing platyPS dependency in CI builds

--- build/travis.ps1
+++ build/travis.ps1
@@ -1,5 +1,6 @@
 # Install InvokeBuild
 Install-Module InvokeBuild -Scope CurrentUser -Force
+Install-Module PlatyPS -Scope CurrentUser -Force
 
 # Build the code and perform tests
 Import-module InvokeBuild
