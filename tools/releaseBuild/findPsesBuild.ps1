65464f190a74c84d3f670de39611c7cce64c6feb
Robert Holt <roholt@microsoft.com>
2018-08-21 19:33:36 -0700

    [Ignore] Add build debug messages

--- tools/releaseBuild/findPsesBuild.ps1
+++ tools/releaseBuild/findPsesBuild.ps1
@@ -1,6 +1,7 @@
 $branch = [uri]::EscapeDataString($env:PSES_BRANCH)
-$buildsUrl = "https://mscodehub.visualstudio.com/PowerShellEditorServices/_apis/build/Builds?definitions=441&branchName=refs%2Fheads%2F$branch&resultFilter=succeeded&reasonFilter=individualCI%20OR%20manual"
+$buildsUrl = "https://mscodehub.visualstudio.com/PowerShellEditorServices/_apis/build/Builds?definitions=441&branchName=refs%2Fheads%2F${branch}&resultFilter=succeeded&reasonFilter=individualCI%20OR%20manual"
 $headers = @{Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN"}
 $builds = Invoke-RestMethod -ContentType application/json -Uri $buildsUrl -Headers $headers
 Write-Host "setting PSES_BUILDID to $builds.value[0].Id"
+Write-Host "Value: $($builds.value[0] | ConvertTo-Json)"
 Write-Host "##vso[task.setvariable variable=PSES_BUILDID]$($builds.value[0].Id)"
