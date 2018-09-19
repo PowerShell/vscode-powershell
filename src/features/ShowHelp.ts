8593ebf40d000205f4e2f2aa877e68b6d4b7e403
corbob <corbobgithub@knoxy.ca>
2018-09-18 20:03:22 -0700

    Remove Dummy command. Cleanup structure. Dummy command was used for testing PSES.

--- src/features/ShowHelp.ts
+++ src/features/ShowHelp.ts
@@ -6,38 +6,18 @@ import vscode = require("vscode");
 import { LanguageClient, NotificationType, RequestType } from "vscode-languageclient";
 import { IFeature } from "../feature";
 
+// TODO: remove ShowOnlineHelpRequest in v2
 export const ShowOnlineHelpRequestType =
     new RequestType<string, void, void, void>("powerShell/showOnlineHelp");
-    // I think we can take out the ShowOnlineHelpRequestType... I don't think we actually send it...
 export const ShowHelpRequestType =
     new RequestType<string, void, void, void>("powerShell/showHelp");
-export class ShowHelpFeature implements IFeature {
 
+export class ShowHelpFeature implements IFeature {
     private command: vscode.Disposable;
     private deprecatedCommand: vscode.Disposable;
-    // TODO: Remove Dummy command before merge.
-    // Dummy command is used to test out the write-warning in PSES
-    // when you send powerShell/showOnlineHelp instead of powerShell/showHelp.
-    private dummyCommand: vscode.Disposable;
     private languageClient: LanguageClient;
 
     constructor() {
-        // TODO: Remove this before merge.
-        this.dummyCommand = vscode.commands.registerCommand("PowerShell.ShowOnlineHelp", () => {
-            if (this.languageClient === undefined) {
-                // TODO: Log error message
-                return;
-            }
-
-            const editor = vscode.window.activeTextEditor;
-
-            const selection = editor.selection;
-            const doc = editor.document;
-            const cwr = doc.getWordRangeAtPosition(selection.active);
-            const text = doc.getText(cwr);
-
-            this.languageClient.sendRequest(ShowOnlineHelpRequestType, text);
-        });
         this.command = vscode.commands.registerCommand("PowerShell.ShowHelp", () => {
             if (this.languageClient === undefined) {
                 // TODO: Log error message
@@ -45,7 +25,6 @@ export class ShowHelpFeature implements IFeature {
             }
 
             const editor = vscode.window.activeTextEditor;
-
             const selection = editor.selection;
             const doc = editor.document;
             const cwr = doc.getWordRangeAtPosition(selection.active);
@@ -63,9 +42,6 @@ export class ShowHelpFeature implements IFeature {
     public dispose() {
         this.command.dispose();
         this.deprecatedCommand.dispose();
-        // TODO: Remove this dummyCommand too...
-        // Don't forget the one in package.json
-        this.dummyCommand.dispose();
     }
 
     public setLanguageClient(languageclient: LanguageClient) {
