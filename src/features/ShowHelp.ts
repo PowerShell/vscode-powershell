ab40a53cadcd4006ef70018691345cd93cc5de01
corbob <30301021+corbob@users.noreply.github.com>
2018-09-20 22:36:37 -0700

    Change "Get Online Help" menu item label: remove Online. (#1516)
    * Change Menu label: remove Online.
    * We now look for help locally if it's not availble online.
    * Rename the file to ShowHelp.
    * Add a command for PowerShell.ShowHelp and mark OnlineHelp as deprecated.
    * Display warning when OnlineHelp is called and then execute ShowHelp.
    * Remove showOnlineHelp requestType as it's not used.

--- src/features/ShowHelp.ts
+++ src/features/ShowHelp.ts
@@ -6,34 +6,40 @@ import vscode = require("vscode");
 import { LanguageClient, NotificationType, RequestType } from "vscode-languageclient";
 import { IFeature } from "../feature";
 
-export const ShowOnlineHelpRequestType =
-    new RequestType<string, void, void, void>("powerShell/showOnlineHelp");
+export const ShowHelpRequestType =
+    new RequestType<string, void, void, void>("powerShell/showHelp");
 
 export class ShowHelpFeature implements IFeature {
-
     private command: vscode.Disposable;
+    private deprecatedCommand: vscode.Disposable;
     private languageClient: LanguageClient;
 
     constructor() {
-        this.command = vscode.commands.registerCommand("PowerShell.OnlineHelp", () => {
+        this.command = vscode.commands.registerCommand("PowerShell.ShowHelp", () => {
             if (this.languageClient === undefined) {
                 // TODO: Log error message
                 return;
             }
 
             const editor = vscode.window.activeTextEditor;
-
             const selection = editor.selection;
             const doc = editor.document;
             const cwr = doc.getWordRangeAtPosition(selection.active);
             const text = doc.getText(cwr);
 
-            this.languageClient.sendRequest(ShowOnlineHelpRequestType, text);
+            this.languageClient.sendRequest(ShowHelpRequestType, text);
+        });
+
+        this.deprecatedCommand = vscode.commands.registerCommand("PowerShell.OnlineHelp", () => {
+            const warnText = "PowerShell.OnlineHelp is being deprecated. Use PowerShell.ShowHelp instead.";
+            vscode.window.showWarningMessage(warnText);
+            vscode.commands.executeCommand("PowerShell.ShowHelp");
         });
     }
 
     public dispose() {
         this.command.dispose();
+        this.deprecatedCommand.dispose();
     }
 
     public setLanguageClient(languageclient: LanguageClient) {
