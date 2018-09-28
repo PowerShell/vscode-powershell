82f2ca227af657362e6b320def08dc3a3d00ed32
Robert Holt <roholt@microsoft.com>
2018-09-27 17:00:44 -0700

    [Ignore] Prepare 1.9.0 release

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ import utils = require("./utils");
 
 // NOTE: We will need to find a better way to deal with the required
 //       PS Editor Services version...
-const requiredEditorServicesVersion = "1.8.5";
+const requiredEditorServicesVersion = "1.9.0";
 
 let logger: Logger;
 let sessionManager: SessionManager;
