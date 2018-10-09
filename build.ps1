3deee9ff557d3885aa0c412fe0b91868e55d9e3c
Robert Holt <rjmholt@gmail.com>
2018-10-08 19:00:31 -0700

    [Ignore] Make build.ps1 work when no params given (#1566)

--- build.ps1
+++ build.ps1
@@ -2,6 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+[CmdletBinding(DefaultParameterSetName = "Build")]
 param(
     [Parameter(ParameterSetName="Bootstrap")]
     [switch]
