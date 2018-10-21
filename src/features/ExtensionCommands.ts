c4db8d7c78c994cfbb78b77142ec059145c08617
Tyler Leonhardt <tylerl0706@gmail.com>
2018-10-21 08:46:46 +0800

    add currentFileLanguage

--- src/features/ExtensionCommands.ts
+++ src/features/ExtensionCommands.ts
@@ -25,6 +25,7 @@ export const InvokeExtensionCommandRequestType =
 
 export interface IEditorContext {
     currentFileContent: string;
+    currentFileLanguage: string;
     currentFilePath: string;
     cursorPosition: Position;
     selectionRange: Range;
@@ -346,6 +347,7 @@ export class ExtensionCommandsFeature implements IFeature {
     private getEditorContext(): IEditorContext {
         return {
             currentFileContent: vscode.window.activeTextEditor.document.getText(),
+            currentFileLanguage: vscode.window.activeTextEditor.document.languageId,
             currentFilePath: vscode.window.activeTextEditor.document.uri.toString(),
             cursorPosition: asPosition(vscode.window.activeTextEditor.selection.active),
             selectionRange:
