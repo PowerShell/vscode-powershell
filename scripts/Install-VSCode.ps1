ede509bc0fb43442cdaa493ad7451eba08e06817
Robert Holt <roholt@microsoft.com>
2018-11-01 11:15:00 -0700

    Fix string template bug

--- scripts/Install-VSCode.ps1
+++ scripts/Install-VSCode.ps1
@@ -579,7 +579,7 @@ try {
     elseif ($IsLinux -or $IsMacOS) {
         # On *nix we need to install extensions as the user -- VSCode refuses root
         $extsSlashes = $extensions -join '/'
-        sudo -H -u $env:SUDO_USER pwsh -c "`$exts = $extsSlashes -split '/'; foreach (`$e in `$exts) { $codeExePath --install-extension `$e }"
+        sudo -H -u $env:SUDO_USER pwsh -c "`$exts = '$extsSlashes' -split '/'; foreach (`$e in `$exts) { $codeExePath --install-extension `$e }"
     }
     else {
         foreach ($extension in $extensions) {
