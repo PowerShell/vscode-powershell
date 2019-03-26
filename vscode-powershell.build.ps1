a226f734975af58fbecabc6c90a20c73d069df12
Tyler Leonhardt <tylerl0706@gmail.com>
2019-03-25 20:53:10 -0700

    add #Requires

--- vscode-powershell.build.ps1
+++ vscode-powershell.build.ps1
@@ -3,6 +3,8 @@
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 #
 
+#Requires -Version 6.0
+
 param(
     [string]$EditorServicesRepoPath = $null
 )
