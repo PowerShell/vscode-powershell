c8363e903f976d35a037c6191b317cf7c4c0d7f0
Tyler Leonhardt <tylerl0706@gmail.com>
2019-03-25 21:02:43 -0700

    #Requires in the correct file

--- tools/Get-PowerShellExtensionChangelog.ps1
+++ tools/Get-PowerShellExtensionChangelog.ps1
@@ -1,6 +1,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+#Requires -Version 6.0
+
 ##############################
 #.SYNOPSIS
 #Generate the draft change log of the PowerShell Extension for VSCode
