96dd4fe86d5376c4f96063b8b73decee7da3cd06
Robert Holt <rjmholt_msft@outlook.com>
2019-04-30 18:03:46 +1000

    Generalize release script to work with PSES

--- tools/postReleaseScripts/publishGHRelease.ps1
+++ tools/postReleaseScripts/publishGHRelease.ps1
@@ -1,8 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-#requires -Version 6.0
-
 param(
     [Parameter(Mandatory)]
     [semver]
@@ -31,17 +29,6 @@ param(
 
 Import-Module "$PSScriptRoot/../GitHubTools.psm1" -Force
 
-<#
-.SYNOPSIS
-Get the release description from the CHANGELOG
-
-.DESCRIPTION
-Gets the latest CHANGELOG entry from the CHANGELOG for use as the GitHub release description
-
-.PARAMETER ChangelogPath
-Path to the changelog file
-#>
-
 function GetDescriptionFromChangelog
 {
     param(
@@ -51,10 +38,7 @@ function GetDescriptionFromChangelog
     )
 
     $lines = Get-Content -Path $ChangelogPath
-    # First two lines are the title and newline
-    # Third looks like '## vX.Y.Z-releasetag'
     $sb = [System.Text.StringBuilder]::new($lines[2])
-    # Read through until the next '## vX.Y.Z-releasetag' H2
     for ($i = 3; -not $lines[$i].StartsWith('## '); $i++)
     {
         $null = $sb.Append("`n").Append($lines[$i])
@@ -76,4 +60,5 @@ $releaseParams = @{
     Description = GetDescriptionFromChangelog -ChangelogPath $ChangelogPath
     GitHubToken = $GitHubToken
 }
-Publish-GitHubRelease @releaseParams
+CreateNewRelease @releaseParams
+
