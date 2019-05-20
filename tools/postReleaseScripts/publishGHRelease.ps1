a7401961967f50f0de0dc64a82718b1ceb9d8ec5
Robert Holt <rjmholt_msft@outlook.com>
2019-05-19 23:40:44 -0700

    [Ignore] GitHub release script (#1937)

---
+++ tools/postReleaseScripts/publishGHRelease.ps1
@@ -0,0 +1,79 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+#requires -Version 6.0
+
+param(
+    [Parameter(Mandatory)]
+    [semver]
+    $Version,
+
+    [Parameter(Mandatory)]
+    [string]
+    $GitHubToken,
+
+    [Parameter(Mandatory)]
+    [string]
+    $Repository,
+
+    [Parameter()]
+    [string]
+    $TargetFork = 'PowerShell',
+
+    [Parameter()]
+    [string]
+    $ChangelogPath = "$PSScriptRoot/../../CHANGELOG.md",
+
+    [Parameter()]
+    [string[]]
+    $AssetPath
+)
+
+Import-Module "$PSScriptRoot/../GitHubTools.psm1" -Force
+
+<#
+.SYNOPSIS
+Get the release description from the CHANGELOG
+
+.DESCRIPTION
+Gets the latest CHANGELOG entry from the CHANGELOG for use as the GitHub release description
+
+.PARAMETER ChangelogPath
+Path to the changelog file
+#>
+
+function GetDescriptionFromChangelog
+{
+    param(
+        [Parameter(Mandatory)]
+        [string]
+        $ChangelogPath
+    )
+
+    $lines = Get-Content -Path $ChangelogPath
+    # First two lines are the title and newline
+    # Third looks like '## vX.Y.Z-releasetag'
+    $sb = [System.Text.StringBuilder]::new($lines[2])
+    # Read through until the next '## vX.Y.Z-releasetag' H2
+    for ($i = 3; -not $lines[$i].StartsWith('## '); $i++)
+    {
+        $null = $sb.Append("`n").Append($lines[$i])
+    }
+
+    return $sb.ToString()
+}
+
+$tag = "v$Version"
+
+$releaseParams = @{
+    Organization = $TargetFork
+    Repository = $Repository
+    Tag = $tag
+    ReleaseName = $tag
+    Branch = "release/$Version"
+    AssetPath = $AssetPath
+    Prerelease = [bool]($Version.PreReleaseLabel)
+    Description = GetDescriptionFromChangelog -ChangelogPath $ChangelogPath
+    GitHubToken = $GitHubToken
+}
+Publish-GitHubRelease @releaseParams
