b9b93ee9bdf5605779cbee609bd2d00d2f291b09
Sydney Smith <43417619+SydneyhSmith@users.noreply.github.com>
2019-07-31 11:54:40 -0700

    Update platform.test.ts

--- test/platform.test.ts
+++ test/platform.test.ts
@@ -77,7 +77,7 @@ suite("Platform module", () => {
             checkDefaultPowerShellPath(
                 platformDetails,
                 platform.getAvailablePowerShellExes(platformDetails, undefined).filter((psPath) => (psPath.versionName)
-                === "PowerShell Core 6"));
+                === "PowerShell Core 6")[0].exePath);
 
             checkAvailableWindowsPowerShellPaths(
                 platformDetails,
