5d509e459770b62be0de8c65935205eb5d3f4fad
Sydney Smith <43417619+SydneyhSmith@users.noreply.github.com>
2019-08-22 20:30:54 -0700

    Backporting #2094 to set default PowerShell to latest available (#2149)
    * Merge pull request #2094 from SydneyhSmith/master
    
    Set Default PowerShell Version on Windows to Latest Available
    
    * Update platform.test.ts
    
    * Update src/platform.ts
    
    Co-Authored-By: Tyler James Leonhardt <tylerl0706@gmail.com>

--- test/platform.test.ts
+++ test/platform.test.ts
@@ -77,7 +77,7 @@ suite("Platform module", () => {
 
         checkDefaultPowerShellPath(
             platformDetails,
-            platform.SysnativePowerShellPath);
+            "C:\\Program Files\\PowerShell\\6\\pwsh.exe");
 
         checkAvailableWindowsPowerShellPaths(
             platformDetails,
@@ -107,7 +107,7 @@ suite("Platform module", () => {
 
         checkDefaultPowerShellPath(
             platformDetails,
-            platform.System32PowerShellPath);
+            "C:\\Program Files\\PowerShell\\6\\pwsh.exe");
 
         checkAvailableWindowsPowerShellPaths(
             platformDetails,
@@ -118,4 +118,86 @@ suite("Platform module", () => {
                 },
             ]);
     });
+    if (process.platform === "win32") {
+        suite("64-bit Windows, 64-bit VS Code", () => {
+            const platformDetails: platform.IPlatformDetails = {
+                operatingSystem: platform.OperatingSystem.Windows,
+                isOS64Bit: true,
+                isProcess64Bit: true,
+            };
+
+            checkDefaultPowerShellPath(
+                platformDetails,
+                platform.System32PowerShellPath);
+
+            checkAvailableWindowsPowerShellPaths(
+                platformDetails,
+                [
+                    {
+                        versionName: platform.WindowsPowerShell64BitLabel,
+                        exePath: platform.System32PowerShellPath,
+                    },
+                    {
+                        versionName: platform.WindowsPowerShell32BitLabel,
+                        exePath: platform.SysWow64PowerShellPath,
+                    },
+                ]);
+
+            checkFixedWindowsPowerShellpath(
+                platformDetails,
+                platform.SysnativePowerShellPath,
+                platform.System32PowerShellPath);
+        });
+
+        suite("64-bit Windows, 32-bit VS Code", () => {
+            const platformDetails: platform.IPlatformDetails = {
+                operatingSystem: platform.OperatingSystem.Windows,
+                isOS64Bit: true,
+                isProcess64Bit: false,
+            };
+
+            checkDefaultPowerShellPath(
+                platformDetails,
+                "C:\\Program Files\\PowerShell\\6\\pwsh.exe");
+
+            checkAvailableWindowsPowerShellPaths(
+                platformDetails,
+                [
+                    {
+                        versionName: platform.WindowsPowerShell64BitLabel,
+                        exePath: platform.SysnativePowerShellPath,
+                    },
+                    {
+                        versionName: platform.WindowsPowerShell32BitLabel,
+                        exePath: platform.System32PowerShellPath,
+                    },
+                ]);
+
+            checkFixedWindowsPowerShellpath(
+                platformDetails,
+                platform.SysWow64PowerShellPath,
+                platform.System32PowerShellPath);
+        });
+
+        suite("32-bit Windows, 32-bit VS Code", () => {
+            const platformDetails: platform.IPlatformDetails = {
+                operatingSystem: platform.OperatingSystem.Windows,
+                isOS64Bit: false,
+                isProcess64Bit: false,
+            };
+
+            checkDefaultPowerShellPath(
+                platformDetails,
+                "C:\\Program Files\\PowerShell\\6\\pwsh.exe");
+
+            checkAvailableWindowsPowerShellPaths(
+                platformDetails,
+                [
+                    {
+                        versionName: platform.WindowsPowerShell32BitLabel,
+                        exePath: platform.System32PowerShellPath,
+                    },
+                ]);
+        });
+    }
 });
