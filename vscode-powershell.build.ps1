43a1816a6ebdfbf5a7bb4bca3b2258a87043ad3c
Tyler Leonhardt <tylerl0706@gmail.com>
2019-09-17 19:05:08 -0700

    help

--- vscode-powershell.build.ps1
+++ vscode-powershell.build.ps1
@@ -33,7 +33,7 @@ task ResolveEditorServicesPath -Before CleanEditorServices, BuildEditorServices,
     }
     else {
         $script:psesRepoPath = Resolve-Path $script:psesRepoPath
-        gci $script:psesRepoPath
+        gci $env:BUILD_SOURCESDIRECTORY
         $script:psesBuildScriptPath = Resolve-Path "$script:psesRepoPath/PowerShellEditorServices.build.ps1"
     }
 }
