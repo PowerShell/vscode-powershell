58d5566a25f39500d02770191bb6791ac1d2a882
Tyler Leonhardt <tylerl0706@gmail.com>
2019-09-19 16:22:18 -0700

    logs

--- tools/releaseBuild/findPsesBuild.ps1
+++ tools/releaseBuild/findPsesBuild.ps1
@@ -3,15 +3,19 @@ $headers = @{Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN"}
 
 $buildsUrl = $env:VSTS_PSES_URL_TEMPLATE -f $branch, "succeeded"
 $succeededBuilds = Invoke-RestMethod -ContentType application/json -Uri $buildsUrl -Headers $headers
+Write-Host "Requested URL: $buildsUrl"
+Write-Host "Got response:`n$(ConvertTo-Json $builds)"
+
 $buildsUrl = $env:VSTS_PSES_URL_TEMPLATE -f $branch, "partiallySucceeded"
 $partiallySucceededBuilds = Invoke-RestMethod -ContentType application/json -Uri $buildsUrl -Headers $headers
+Write-Host "Requested URL: $buildsUrl"
+Write-Host "Got response:`n$(ConvertTo-Json $builds)"
+
 $builds = @(
     $succeededBuilds.value
     $partiallySucceededBuilds.value
     ) | Sort-Object finishTime -Descending
 
 Write-Host "Got PSES_BRANCH: ${env:PSES_BRANCH}"
-Write-Host "Requested URL: $buildsUrl"
-Write-Host "Got response:`n$(ConvertTo-Json $builds)"
 Write-Host "setting PSES_BUILDID to $($builds.value[0].Id)"
 Write-Host "##vso[task.setvariable variable=PSES_BUILDID]$($builds.value[0].Id)"
