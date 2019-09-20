75d313802a7861a3898359db05272232d7f3a0f1
Tyler James Leonhardt <tylerl0706@gmail.com>
2019-09-20 10:10:55 -0700

    [Ignore] Move to yml legacy (#2192)
    * initial release yml
    
    * update path
    
    * move build to artifact
    
    * rest of changes
    
    * temp for building
    
    * specific artifact
    
    * move back to legacy/1.x
    
    * single

--- tools/releaseBuild/findPsesBuild.ps1
+++ tools/releaseBuild/findPsesBuild.ps1
@@ -1,9 +1,21 @@
 $branch = [uri]::EscapeDataString($env:PSES_BRANCH)
-$buildsUrl = $env:VSTS_PSES_URL_TEMPLATE -f $branch
 $headers = @{Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN"}
-$builds = Invoke-RestMethod -ContentType application/json -Uri $buildsUrl -Headers $headers
-Write-Host "Got PSES_BRANCH: ${env:PSES_BRANCH}"
+
+$buildsUrl = $env:VSTS_PSES_URL_TEMPLATE -f $branch, "succeeded"
+$succeededBuilds = Invoke-RestMethod -ContentType application/json -Uri $buildsUrl -Headers $headers
 Write-Host "Requested URL: $buildsUrl"
-Write-Host "Got response:`n$(ConvertTo-Json $builds)"
-Write-Host "setting PSES_BUILDID to $($builds.value[0].Id)"
-Write-Host "##vso[task.setvariable variable=PSES_BUILDID]$($builds.value[0].Id)"
+Write-Host "Got response:`n$(ConvertTo-Json $succeededBuilds)"
+
+$buildsUrl = $env:VSTS_PSES_URL_TEMPLATE -f $branch, "partiallySucceeded"
+$partiallySucceededBuilds = Invoke-RestMethod -ContentType application/json -Uri $buildsUrl -Headers $headers
+Write-Host "Requested URL: $buildsUrl"
+Write-Host "Got response:`n$(ConvertTo-Json $partiallySucceededBuilds)"
+
+$builds = @(
+    $succeededBuilds.value
+    $partiallySucceededBuilds.value
+    ) | Sort-Object finishTime -Descending
+
+Write-Host "Got PSES_BRANCH: ${env:PSES_BRANCH}"
+Write-Host "setting PSES_BUILDID to $($builds[0].Id)"
+Write-Host "##vso[task.setvariable variable=PSES_BUILDID]$($builds[0].Id)"
--- vscode-powershell.build.ps1
+++ vscode-powershell.build.ps1
@@ -188,11 +188,15 @@ task Package UpdateReadme, {
 
     # Change the package to have a static name for automation purposes
     Move-Item -Force .\$($script:PackageJson.name)-$($script:PackageJson.version).vsix .\PowerShell-insiders.vsix
+
+    if ($env:TF_BUILD) {
+        Copy-Item -Path PowerShell-insiders.vsix -Destination $env:BUILD_ARTIFACTSTAGINGDIRECTORY
+    }
 }
 
 #endregion
 
 # The set of tasks for a release
-task Release Clean, Build, Test, Package
+task Release Clean, Build, Package
 # The default task is to run the entire CI build
 task . Clean, BuildAll, Test, UpdatePackageJson, Package, UploadArtifacts
