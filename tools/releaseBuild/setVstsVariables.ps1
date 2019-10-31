2ea9252466d9ebd0ab3d2228b3d2d1b7eed72835
Robert Holt <rjmholt_msft@outlook.com>
2019-10-31 16:14:18 -0700

    [Ignore] Allow VSTS env var override (#2264)

--- tools/releaseBuild/setVstsVariables.ps1
+++ tools/releaseBuild/setVstsVariables.ps1
@@ -5,6 +5,12 @@ $vstsVariables = @{
 # Use VSTS's API to set an env vars
 foreach ($var in $vstsVariables.Keys)
 {
+    # Allow environment to override
+    if (Get-Item "env:$var" -ErrorAction Ignore)
+    {
+        continue
+    }
+
     $val = $vstsVariables[$var]
     Write-Host "Setting var '$var' to value '$val'"
     Write-Host "##vso[task.setvariable variable=$var]$val"
