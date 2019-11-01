22bcc2c11c3b69b5eb0667492eb677c5f84f74f4
Robert Holt <rjmholt_msft@outlook.com>
2019-11-01 11:48:56 -0700

    [Ignore] Buildfix backport (#2267)
    * Fix node modules in credscan
    
    * Allow branch target override
    
    * Downgrade vsce

--- tools/releaseBuild/setVstsVariables.ps1
+++ tools/releaseBuild/setVstsVariables.ps1
@@ -5,6 +5,11 @@ $vstsVariables = @{
 # Use VSTS's API to set an env vars
 foreach ($var in $vstsVariables.Keys)
 {
+    if (Get-Item "env:$var" -ErrorAction Ignore)
+    {
+        continue
+    }
+
     $val = $vstsVariables[$var]
     Write-Host "Setting var '$var' to value '$val'"
     Write-Host "##vso[task.setvariable variable=$var]$val"
