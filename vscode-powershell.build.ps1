60cc299354bde527e917704adac0adc4008b51a6
Tyler Leonhardt <tylerl0706@gmail.com>
2019-12-12 15:20:37 -0800

    try to run on linux again

--- vscode-powershell.build.ps1
+++ vscode-powershell.build.ps1
@@ -101,10 +101,6 @@ task BuildAll BuildEditorServices, Build
 #region Test tasks
 
 task Test Build, {
-    if ($env:TF_BUILD -and $global:IsLinux) {
-        Write-Warning "Skipping extension tests in Linux CI because vscode does not support it."
-        return
-    }
     Write-Host "`n### Running extension tests" -ForegroundColor Green
     exec { & npm run test }
 }
