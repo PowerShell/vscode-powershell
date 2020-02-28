f1f46caa4abb1d44e6aef607b762b58428821d33
Robert Holt <rjmholt_msft@outlook.com>
2020-02-27 21:15:35 -0800

    [Ignore] Update vsce to use auto link opt out (#2500)

--- vscode-powershell.build.ps1
+++ vscode-powershell.build.ps1
@@ -187,7 +187,7 @@ task Package UpdateReadme, {
     }
 
     Write-Host "`n### Packaging PowerShell-insiders.vsix`n" -ForegroundColor Green
-    exec { & node ./node_modules/vsce/out/vsce package }
+    exec { & node ./node_modules/vsce/out/vsce package --noGitHubIssueLinking }
 
     # Change the package to have a static name for automation purposes
     Move-Item -Force .\$($script:PackageJson.name)-$($script:PackageJson.version).vsix .\PowerShell-insiders.vsix
