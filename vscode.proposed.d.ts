c84b82eaa3b4df02c88fade8242328084e32ed47
TylerLeonhardt <TylerLeonhardt@users.noreply.github.com>
2020-08-26 22:02:46 +0000

    [Ignore] Update Notebook dts

--- vscode.proposed.d.ts
+++ vscode.proposed.d.ts
@@ -526,7 +526,19 @@ declare module 'vscode' {
 	export namespace notebook {
 		export function registerNotebookContentProvider(
 			notebookType: string,
-			provider: NotebookContentProvider
+			provider: NotebookContentProvider,
+			options?: {
+				/**
+				 * Controls if outputs change will trigger notebook document content change and if it will be used in the diff editor
+				 * Default to false. If the content provider doesn't persisit the outputs in the file document, this should be set to true.
+				 */
+				transientOutputs: boolean;
+				/**
+				 * Controls if a meetadata property change will trigger notebook document content change and if it will be used in the diff editor
+				 * Default to false. If the content provider doesn't persisit a metadata property in the file document, it should be set to true.
+				 */
+				transientMetadata: { [K in keyof NotebookCellMetadata]?: boolean }
+			}
 		): Disposable;
 
 		export function registerNotebookKernelProvider(
