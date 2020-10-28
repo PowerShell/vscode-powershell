79c0ae364a2af6e2e7d198a9f0ea54249ee626a5
Tyler James Leonhardt <tylerl0706@gmail.com>
2020-10-27 18:59:48 -0700

    help completion should only run on powershell scripts (#3023)

--- src/features/HelpCompletion.ts
+++ src/features/HelpCompletion.ts
@@ -56,6 +56,11 @@ export class HelpCompletionFeature extends LanguageClientConsumer {
     }
 
     public async onEvent(changeEvent: TextDocumentChangeEvent): Promise<void> {
+        // If it's not a PowerShell script, we don't care about it.
+        if (changeEvent.document.languageId !== "powershell") {
+            return;
+        }
+
         if (!(changeEvent && changeEvent.contentChanges)) {
             this.log.writeWarning(`<${HelpCompletionFeature.name}>: ` +
                 `Bad TextDocumentChangeEvent message: ${JSON.stringify(changeEvent)}`);
