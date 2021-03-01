b7ab01866a28accef80c95eee81c18ea7c71eaf2
Nick Kimbrough <nickkimbrough@gmail.com>
2021-03-01 12:54:36 -0700

    Update examples-module to use Pester 5 (#3214)
    This resolves #3049. Only real changes needed were wrapping the initialization in a
    `BeforeAll` block and changing `Be` to `-Be`.

--- examples/Tests/SampleModule.Tests.ps1
+++ examples/Tests/SampleModule.Tests.ps1
@@ -1,9 +1,10 @@
-$ModuleManifestName = 'SampleModule.psd1'
-Import-Module $PSScriptRoot\..\$ModuleManifestName
-
+BeforeAll {
+    $ModuleManifestName = 'SampleModule.psd1'
+    Import-Module $PSScriptRoot\..\$ModuleManifestName
+}
 Describe 'Module Manifest Tests' {
     It 'Passes Test-ModuleManifest' {
         Test-ModuleManifest -Path $PSScriptRoot\..\$ModuleManifestName
-        $? | Should Be $true
+        $? | Should -Be $true
     }
 }
