bf2846e020a99cf36aa1049225a9a6839fbefed9
Matti <matti@mkdr.net>
2021-06-02 18:05:48 +0300

    Changed literal English check of OS-architecture to a regex that should work on most languages

--- scripts/Install-VSCode.ps1
+++ scripts/Install-VSCode.ps1
@@ -170,7 +170,7 @@ gpgkey=https://packages.microsoft.com/keys/microsoft.asc
 
 function Test-IsOsArchX64 {
     if ($PSVersionTable.PSVersion.Major -lt 6) {
-        return (Get-CimInstance -ClassName Win32_OperatingSystem).OSArchitecture -eq '64-bit'
+        return (Get-CimInstance -ClassName Win32_OperatingSystem).OSArchitecture -match '64'
     }
 
     return [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture -eq [System.Runtime.InteropServices.Architecture]::X64
