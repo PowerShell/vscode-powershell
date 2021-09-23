04ece6dec92ab3e438b0530c5fa99c9dc894b754
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2021-09-23 11:28:40 -0700

    Improve ISE compatibility tests
    Save and restore the user's theme, which was annoying when running the
    tests via Code's debugger. Don't use the default theme "Dark+" so that
    the setting is actually propogated.

--- test/features/ISECompatibility.test.ts
+++ test/features/ISECompatibility.test.ts
@@ -3,14 +3,28 @@
 
 import * as assert from "assert";
 import * as vscode from "vscode";
-import { suiteSetup, setup, teardown } from "mocha";
+import { suiteSetup, setup, suiteTeardown, teardown } from "mocha";
 import { ISECompatibilityFeature } from "../../src/features/ISECompatibility";
 import utils = require("../utils");
 
 suite("ISECompatibility feature", () => {
-    suiteSetup(utils.ensureExtensionIsActivated);
+    let currentTheme: string;
+
+    suiteSetup(async () => {
+        // Save user's current theme.
+        currentTheme = await vscode.workspace.getConfiguration("workbench").get("colorTheme");
+        await utils.ensureExtensionIsActivated();
+    });
+
     setup(async () => { await vscode.commands.executeCommand("PowerShell.EnableISEMode"); });
+
     teardown(async () => { await vscode.commands.executeCommand("PowerShell.DisableISEMode"); });
+
+    suiteTeardown(async () => {
+        // Reset user's current theme.
+        await vscode.workspace.getConfiguration("workbench").update("colorTheme", currentTheme, true);
+        assert.strictEqual(vscode.workspace.getConfiguration("workbench").get("colorTheme"), currentTheme);
+    });
 
     test("It sets ISE Settings", async () => {
         for (const iseSetting of ISECompatibilityFeature.settings) {
@@ -31,15 +45,18 @@ suite("ISECompatibility feature", () => {
         }
     }).timeout(10000);
 
-    test("It leaves Theme after being changed after enabling ISE Mode", async () => {
+    test("It doesn't change theme when disabled if theme was manually changed after being enabled", async () => {
         assert.strictEqual(vscode.workspace.getConfiguration("workbench").get("colorTheme"), "PowerShell ISE");
 
-        await vscode.workspace.getConfiguration("workbench").update("colorTheme", "Dark+", true);
+        // "Manually" change theme after enabling ISE mode. Use a built-in theme but not the default.
+        await vscode.workspace.getConfiguration("workbench").update("colorTheme", "Monokai", true);
+        assert.strictEqual(vscode.workspace.getConfiguration("workbench").get("colorTheme"), "Monokai");
+
         await vscode.commands.executeCommand("PowerShell.DisableISEMode");
         for (const iseSetting of ISECompatibilityFeature.settings) {
             const currently = vscode.workspace.getConfiguration(iseSetting.path).get(iseSetting.name);
             assert.notStrictEqual(currently, iseSetting.value);
         }
-        assert.strictEqual(vscode.workspace.getConfiguration("workbench").get("colorTheme"), "Dark+");
+        assert.strictEqual(vscode.workspace.getConfiguration("workbench").get("colorTheme"), "Monokai");
     }).timeout(10000);
 });
