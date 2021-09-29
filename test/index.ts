75795bc1969616fd062347651a1dfeeeea7d1bfc
Andy Schwartzmeyer <andrew@schwartzmeyer.com>
2021-09-29 11:33:19 -0700

    Remove `mocha-junit-reporter` package by relying on Mocha's xUnit module (#3593)
    This important because the removed package (`mocha-junit-reporter`)
    is quite out-of-date and pulls in a vulnerable dependency.
    Funnily, we're still producing JUnit-style XML test results,
    which is a very curious bug in Mocha itself.

--- test/index.ts
+++ test/index.ts
@@ -16,23 +16,23 @@ export function run(): Promise<void> {
         reporter: "mocha-multi-reporters",
         timeout: 5000,
         reporterOptions: {
-            reporterEnabled: "spec, mocha-junit-reporter",
-            mochaJunitReporterReporterOptions: {
-                mochaFile: path.join(__dirname, "..", "..", "test-results.xml"),
-            },
+            // NOTE: The XML output by Mocha's xUnit reporter is actually in the
+            // JUnit style. I'm unsure how no one else has noticed this.
+            reporterEnabled: "spec, xunit",
+            xunitReporterOptions: {
+                output: path.join(__dirname, "..", "..", "test-results.xml"),
+            }
         },
     });
 
-    const testsRoot = path.resolve(__dirname, "..");
-
     return new Promise((c, e) => {
-        glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
+        glob("**/**.test.js", { cwd: __dirname }, (err, files) => {
             if (err) {
                 return e(err);
             }
 
             // Add files to the test suite
-            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+            files.forEach(f => mocha.addFile(path.resolve(__dirname, f)));
 
             try {
                 // Run the mocha test
