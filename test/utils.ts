57b1749ae36c6cf8455c099a496d9b623c3f7fb9
Andy Schwartzmeyer <andrew@schwartzmeyer.com>
2021-11-09 16:44:36 -0800

    Improve test stability with `ensureEditorServicesIsConnected` (#3643)
    This uses a newly added `ExternalApi` function `waitUntilStarted` that
    ensures the LSP server (PowerShell session) is started after manually
    activating the extension.

--- test/utils.ts
+++ test/utils.ts
@@ -5,6 +5,7 @@
 
 import * as path from "path";
 import * as vscode from "vscode";
+import { IPowerShellExtensionClient } from "../src/features/ExternalApi";
 
 // This lets us test the rest of our path assumptions against the baseline of
 // this test file existing at `<root>/out/test/utils.js`.
@@ -17,4 +18,12 @@ export async function ensureExtensionIsActivated(): Promise<vscode.Extension<any
     const extension = vscode.extensions.getExtension(extensionId);
     if (!extension.isActive) { await extension.activate(); }
     return extension;
+}
+
+export async function ensureEditorServicesIsConnected(): Promise<void> {
+    const powershellExtension = await ensureExtensionIsActivated();
+    const client = powershellExtension!.exports as IPowerShellExtensionClient;
+    const sessionId = client.registerExternalExtension(extensionId);
+    await client.waitUntilStarted(sessionId);
+    client.unregisterExternalExtension(sessionId);
 }
