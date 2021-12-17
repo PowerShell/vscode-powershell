63bbb2f4d666548eae4d616a8bfe84741498fbd8
Andy Schwartzmeyer <andrew@schwartzmeyer.com>
2021-12-17 15:37:17 -0800

    Support debugging without a workspace (#3728)
    The existing code assumed there was always at least one workspace
    folder, but this led to an error if there were zero. This can be
    reproduced by calling "Close Workspace" if one is open and then running
    Pester tests. Since the `vscode.debug.startDebugging()` interface does
    not require a workspace folder, and in fact expects "undefined" if none
    is available, we simply use TypeScript's "optional chaining" (AKA
    conditional access) operator to return undefined if `workspaceFolders`
    is null, and otherwise return the first workspace folder.

--- src/features/PesterTests.ts
+++ src/features/PesterTests.ts
@@ -144,6 +144,6 @@ export class PesterTestsFeature implements vscode.Disposable {
         // Ensure the necessary script exists (for testing). The debugger will
         // start regardless, but we also pass its success along.
         return utils.fileExists(this.invokePesterStubScriptPath)
-            && vscode.debug.startDebugging(vscode.workspace.workspaceFolders[0], launchConfig);
+            && vscode.debug.startDebugging(vscode.workspace.workspaceFolders?.[0], launchConfig);
     }
 }
