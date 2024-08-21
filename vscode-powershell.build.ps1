86679374a4e86c3dcc80c58088874c87e96fcb3b
Andy Jordan <2226434+andyleejordan@users.noreply.github.com>
2024-08-21 13:23:00 -0700

    Reorganize build and packages to allow for omitting dev tools
    New requirements in our release pipeline require us to omit the installation
    of quite a few of our (optional) dev tools, as their post-install tasks
    reach out to the public NPM registry and so are blocked. By moving everything
    except `esbuild` and `vsce` to `optionalDependencies` (and reorganzing the
    build script) we can run the linter and tests outside of the secure container.
    
    Since OneBranch and GitHub Actions are now using PowerShell 7.4, the module
    setup script had to be updated. Because OneBranch now runs without releasing,
    the downloaded drop had to change (skipping a zip and extraction).

--- vscode-powershell.build.ps1
+++ vscode-powershell.build.ps1
@@ -17,20 +17,19 @@ function Get-EditorServicesPath {
     }
     # NOTE: The ErrorActionPreference for both Invoke-Build and Azure DevOps
     # scripts is Stop, but we want to continue and return false here.
-    return Resolve-Path "$psesRepoPath/PowerShellEditorServices.build.ps1" -ErrorAction Continue
+    return Resolve-Path "$psesRepoPath/PowerShellEditorServices.build.ps1" -ErrorAction SilentlyContinue
 }
 
 #region Setup tasks
 
-task RestoreNodeModules -If { !(Test-Path ./node_modules) } {
-    Write-Host "`n### Restoring vscode-powershell dependencies`n" -ForegroundColor Green
-    # When in a CI build use the --loglevel=error parameter so that
-    # package install warnings don't cause PowerShell to throw up
-    if ($env:CI -or $env:TF_BUILD) {
-        Invoke-BuildExec { & npm ci --loglevel=error }
-    } else {
-        Invoke-BuildExec { & npm install }
-    }
+task RestoreNode -If { !(Test-Path ./node_modules/esbuild) } {
+    Write-Build DarkGreen "Restoring build dependencies"
+    Invoke-BuildExec { & npm ci --omit=optional }
+}
+
+task RestoreNodeOptional -If { !(Test-Path ./node_modules/eslint) } {
+    Write-Build DarkMagenta "Restoring build, test, and lint dependencies"
+    Invoke-BuildExec { & npm ci --include=optional }
 }
 
 task RestoreEditorServices -If (Get-EditorServicesPath) {
@@ -39,60 +38,59 @@ task RestoreEditorServices -If (Get-EditorServicesPath) {
             # When debugging, we always rebuild PSES and ensure its symlinked so
             # that developers always have the latest local bits.
             if ((Get-Item ./modules -ErrorAction SilentlyContinue).LinkType -ne "SymbolicLink") {
-                Write-Host "`n### Creating symbolic link to PSES" -ForegroundColor Green
+                Write-Build DarkMagenta "Creating symbolic link to PSES"
                 Remove-BuildItem ./modules
                 New-Item -ItemType SymbolicLink -Path ./modules -Target "$(Split-Path (Get-EditorServicesPath))/module"
             }
 
-            Write-Host "`n### Building PSES`n" -ForegroundColor Green
+            Write-Build DarkGreen "Building PSES"
             Invoke-Build Build (Get-EditorServicesPath) -Configuration $Configuration
         }
         "Release" {
             # When releasing, we ensure the bits are not symlinked but copied,
             # and only if they don't already exist.
             if ((Get-Item ./modules -ErrorAction SilentlyContinue).LinkType -eq "SymbolicLink") {
-                Write-Host "`n### Deleting PSES symbolic link" -ForegroundColor Green
+                Write-Build DarkRed "Deleting PSES symbolic link"
                 Remove-BuildItem ./modules
             }
 
             if (!(Test-Path ./modules)) {
                 # We only build if it hasn't been built at all.
                 if (!(Test-Path "$(Split-Path (Get-EditorServicesPath))/module/PowerShellEditorServices/bin")) {
-                    Write-Host "`n### Building PSES`n" -ForegroundColor Green
+                    Write-Build DarkGreen "Building PSES"
                     Invoke-Build Build (Get-EditorServicesPath) -Configuration $Configuration
                 }
 
-                Write-Host "`n### Copying PSES`n" -ForegroundColor Green
+                Write-Build DarkGreen "Copying PSES"
                 Copy-Item -Recurse -Force "$(Split-Path (Get-EditorServicesPath))/module" ./modules
             }
         }
     }
 }
 
-task Restore RestoreEditorServices, RestoreNodeModules
-
 #endregion
 #region Clean tasks
 
 task Clean {
-    Write-Host "`n### Cleaning vscode-powershell`n" -ForegroundColor Green
+    Write-Build DarkMagenta "Cleaning vscode-powershell"
     Remove-BuildItem *.js, *.js.map, ./dist, ./modules, ./node_modules, ./out
 }
 
 task CleanEditorServices -If (Get-EditorServicesPath) {
-    Write-Host "`n### Cleaning PowerShellEditorServices`n" -ForegroundColor Green
+    Write-Build DarkMagenta "Cleaning PowerShellEditorServices"
     Invoke-Build Clean (Get-EditorServicesPath)
 }
 
 #endregion
 #region Build tasks
-
-task Build Restore, {
-    Write-Host "`n### Building vscode-powershell`n" -ForegroundColor Green
-    Assert-Build (Test-Path ./modules/PowerShellEditorServices/bin) "Extension requires PSES"
-
-    Write-Host "`n### Linting TypeScript`n" -ForegroundColor Green
+task Lint RestoreNodeOptional, {
+    Write-Build DarkMagenta "Linting TypeScript"
     Invoke-BuildExec { & npm run lint }
+}
+
+task Build RestoreEditorServices, RestoreNode, {
+    Write-Build DarkGreen "Building vscode-powershell"
+    Assert-Build (Test-Path ./modules/PowerShellEditorServices/bin) "Extension requires PSES"
 
     # TODO: When supported we should use `esbuild` for the tests too. Although
     # we now use `esbuild` to transpile, bundle, and minify the extension, we
@@ -109,15 +107,15 @@ task Build Restore, {
 #endregion
 #region Test tasks
 
-task Test Build, {
-    Write-Host "`n### Running extension tests" -ForegroundColor Green
+task Test Lint, Build, {
+    Write-Build DarkMagenta "Running extension tests"
     Invoke-BuildExec { & npm run test }
     # Reset the state of files modified by tests
     Invoke-BuildExec { git checkout package.json test/TestEnvironment.code-workspace }
 }
 
 task TestEditorServices -If (Get-EditorServicesPath) {
-    Write-Host "`n### Testing PowerShellEditorServices`n" -ForegroundColor Green
+    Write-Build DarkMagenta "Testing PowerShellEditorServices"
     Invoke-Build Test (Get-EditorServicesPath)
 }
 
@@ -126,7 +124,7 @@ task TestEditorServices -If (Get-EditorServicesPath) {
 
 task Package {
     [semver]$version = $((Get-Content -Raw -Path package.json | ConvertFrom-Json).version)
-    Write-Host "`n### Packaging powershell-$version.vsix`n" -ForegroundColor Green
+    Write-Build DarkGreen "Packaging powershell-$version.vsix"
     Remove-BuildItem ./out
     New-Item -ItemType Directory -Force out | Out-Null
 
@@ -136,13 +134,13 @@ task Package {
     # we might have built in Debug configuration, not Release, and still want to
     # package it. So delete the symlink and copy what we just built.
     if ((Get-Item ./modules -ErrorAction SilentlyContinue).LinkType -eq "SymbolicLink") {
-        Write-Host "`n### PSES is a symbolic link, replacing with copy!" -ForegroundColor Green
+        Write-Build DarkRed "PSES is a symbolic link, replacing with copy!"
         Remove-BuildItem ./modules
         Copy-Item -Recurse -Force "$(Split-Path (Get-EditorServicesPath))/module" ./modules
     }
 
     if ($version.Minor % 2 -ne 0) {
-        Write-Host "`n### This is a pre-release!`n" -ForegroundColor Green
+        Write-Build DarkRed "This is a pre-release!"
         Invoke-BuildExec { & npm run package -- --pre-release }
     } else {
         Invoke-BuildExec { & npm run package }
@@ -151,4 +149,4 @@ task Package {
 
 #endregion
 
-task . Build, Test, Package
+task . Test, Package
